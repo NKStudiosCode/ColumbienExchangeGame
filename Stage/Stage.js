7d89e9e77a86d407c6a80ae48c07f31918897fb0
NKStudiosCode <119743006+NKStudiosCode@users.noreply.github.com>
2022-12-03 10:16:20 -0500

    Add files via upload

---
+++ Stage/Stage.js
@@ -0,0 +1,26 @@
+/* eslint-disable require-yield, eqeqeq */
+
+import {
+  Stage as StageBase,
+  Trigger,
+  Watcher,
+  Costume,
+  Color,
+  Sound
+} from "https://unpkg.com/leopard@^1/dist/index.esm.js";
+
+export default class Stage extends StageBase {
+  constructor(...args) {
+    super(...args);
+
+    this.costumes = [
+      new Costume("download", "./Stage/costumes/download.png", { x: 0, y: 0 })
+    ];
+
+    this.sounds = [new Sound("pop", "./Stage/sounds/pop.wav")];
+
+    this.triggers = [];
+
+    this.vars.myVariable = 0;
+  }
+}
