c5f54a06fd5fa7a13451beef10f40c0d56189f27
NKStudiosCode <119743006+NKStudiosCode@users.noreply.github.com>
2022-12-03 11:30:46 -0500

    Update index.js

--- index.js
+++ index.js
@@ -38,6 +38,6 @@ const sprites = {
 };
 
 const project = new Project(stage, sprites, {
-  frameRate: 30 // Set to 60 to make your project run faster
+  frameRate: 120 // Set to 60 to make your project run faster
 });
 export default project;
